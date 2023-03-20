d8f860f3ef2cbf80cc5826e4ec4b11581debc78a
Khaled Allam <kaaoaa505@gmail.com>
2023-03-20 10:13:16 +0300

    enable form inputs data submit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const Logger = require('./middlewares/Logger');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.send('Home page')
